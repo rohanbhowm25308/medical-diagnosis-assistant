94b6222a82dfb1d40414504531a6742f5538e28c
rohanbhowm25308 <bhowmikrohan83@gmail.com>
2026-07-06 21:26:54 +0530

    Fixed project structure for Render deployment

--- Documents/Sentiment-Analysis-Dashboard/utils/preprocess.py
+++
@@ -1,8 +0,0 @@
-import re
-import string
-
-def clean_text(text):
-    text = text.lower()
-    text = re.sub(f"[{string.punctuation}]", "", text)
-    text = re.sub(r"\d+", "", text)
-    return text
