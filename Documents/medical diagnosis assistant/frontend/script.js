9fa4ef356465fd3853aea1ac7fb5bd3d1521933b
rohanbhowm25308 <bhowmikrohan83@gmail.com>
2026-07-07 13:55:43 +0530

    Updated API URL for Render

--- Documents/medical diagnosis assistant/frontend/script.js
+++ Documents/medical diagnosis assistant/frontend/script.js
@@ -53,7 +53,7 @@ async function predictDisease() {
     };
     console.log(symptoms);
 
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://medical-diagnosis-assistant-79xo.onrender.com/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
